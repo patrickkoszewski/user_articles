1689251d3ace902979fcd32f675580d8e1c2c115
Patrick <ledrap1992@gmail.com>
2023-01-18 18:46:03 +0100

    Add Get_It

--- lib/data/remote_data_sources/authors_remote_data_source.dart
+++ lib/data/remote_data_sources/authors_remote_data_source.dart
@@ -12,18 +12,3 @@ abstract class AuthorsRemoteRetrofitDataSource {
   @GET("/users")
   Future<List<AuthorModel>> getAuthors();
 }
-
-// //przychodzi nam lista [] elementów
-// class AuthorsRemoteDioDataSource {
-//   Future<List<Map<String, dynamic>>?> getAuthors() async {
-//     final response = await Dio().get<List<dynamic>>(
-//         'http://my-json-server.typicode.com/adamsmaka/json-demo/users');
-//     final listDynamic = response.data;
-//     if (listDynamic == null) {
-//       return null;
-//     }
-//     // przechodzimy przez wszystkie dynamic i konwerttujemy go by
-//     // zwracało nam liste której potrzebujemy <List<Map<String, dynamic>>
-//     return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-//   }
-// }
--- lib/features/home/page/home_page.dart
+++ lib/features/home/page/home_page.dart
@@ -1,10 +1,8 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_articles/app/core/enums.dart';
-import 'package:user_articles/data/remote_data_sources/authors_remote_data_source.dart';
+import 'package:user_articles/app/injection_container.dart';
 import 'package:user_articles/domain/models/author_model.dart';
-import 'package:user_articles/domain/repositories/authors_repository.dart';
 import 'package:user_articles/features/articles/page/articles_page.dart';
 import 'package:user_articles/features/home/cubit/home_cubit.dart';
 
@@ -13,16 +11,13 @@ class HomePage extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  // w Bloc provider jest <HomeCubit> gdyby nie było dajemy w getIt<HomeCubit>()
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: BlocProvider<HomeCubit>(
         create: (context) {
-          return HomeCubit(
-            authorsRepository: AuthorsRepository(
-              remoteDataSource: AuthorsRemoteRetrofitDataSource(Dio()),
-            ),
-          )..start();
+          return getIt()..start();
         },
         child: BlocBuilder<HomeCubit, HomeState>(
           builder: (context, state) {
