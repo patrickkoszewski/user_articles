8ecb0ffcd377894641c47cbe88867a2ef1045c33
Patrick <ledrap1992@gmail.com>
2023-01-18 15:56:04 +0100

    Cleaning Code

--- lib/domain/models/article_model.dart
+++ lib/domain/models/article_model.dart
@@ -14,28 +14,3 @@ class ArticleModel with _$ArticleModel {
   factory ArticleModel.fromJson(Map<String, dynamic> json) =>
       _$ArticleModelFromJson(json);
 }
-
-// @JsonSerializable()
-// class ArticleModel {
-//   const ArticleModel({
-//     required this.id,
-//     required this.authorId,
-//     required this.content,
-//   });
-
-//   //gdy key z backednu ma taką samą nazwę nie musimy definiować JsonKey
-//   //usunięte bo takie same @JsonKey(name: 'id')
-//   final int id;
-
-//   //tutaj się różni więc musi zostać
-//   @JsonKey(name: 'author_id')
-//   final int authorId;
-
-//   //@JsonKey(name: 'content')
-//   final String content;
-
-//   factory ArticleModel.fromJson(Map<String, dynamic> json) =>
-//       _$ArticleModelFromJson(json);
-
-//   Map<String, dynamic> toJson() => _$ArticleModelToJson(this);
-// }
--- lib/domain/models/author_model.dart
+++ lib/domain/models/author_model.dart
@@ -20,30 +20,3 @@ class AuthorModel with _$AuthorModel {
   factory AuthorModel.fromJson(Map<String, dynamic> json) =>
       _$AuthorModelFromJson(json);
 }
-
-// @JsonSerializable()
-// class AuthorModel {
-//   const AuthorModel({
-//     required this.id,
-//     required this.picture,
-//     required this.firstName,
-//     required this.lastName,
-//   });
-
-//   final int id;
-
-//   final String picture;
-//   @JsonKey(name: 'first_name')
-//   final String firstName;
-//   @JsonKey(name: 'last_name')
-//   final String lastName;
-
-//   String get name {
-//     return '$firstName $lastName';
-//   }
-
-//   factory AuthorModel.fromJson(Map<String, dynamic> json) =>
-//       _$AuthorModelFromJson(json);
-
-//   Map<String, dynamic> toJson() => _$AuthorModelToJson(this);
-// }
