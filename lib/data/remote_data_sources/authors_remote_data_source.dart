f4cc883937c44d6853f01be96aa3d4d297f84fd5
Patrick <ledrap1992@gmail.com>
2023-01-18 17:00:19 +0100

    Add Retrofit(auto dio)

--- lib/data/remote_data_sources/authors_remote_data_source.dart
+++ lib/data/remote_data_sources/authors_remote_data_source.dart
@@ -1,16 +1,29 @@
 import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:user_articles/domain/models/author_model.dart';
 
-//przychodzi nam lista [] elementów
-class AuthorsRemoteDioDataSource {
-  Future<List<Map<String, dynamic>>?> getAuthors() async {
-    final response = await Dio().get<List<dynamic>>(
-        'http://my-json-server.typicode.com/adamsmaka/json-demo/users');
-    final listDynamic = response.data;
-    if (listDynamic == null) {
-      return null;
-    }
-    // przechodzimy przez wszystkie dynamic i konwerttujemy go by
-    // zwracało nam liste której potrzebujemy <List<Map<String, dynamic>>
-    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-  }
+part 'authors_remote_data_source.g.dart';
+
+@RestApi(baseUrl: "http://my-json-server.typicode.com/adamsmaka/json-demo")
+abstract class AuthorsRemoteRetrofitDataSource {
+  factory AuthorsRemoteRetrofitDataSource(Dio dio, {String baseUrl}) =
+      _AuthorsRemoteRetrofitDataSource;
+
+  @GET("/users")
+  Future<List<AuthorModel>> getAuthors();
 }
+
+// //przychodzi nam lista [] elementów
+// class AuthorsRemoteDioDataSource {
+//   Future<List<Map<String, dynamic>>?> getAuthors() async {
+//     final response = await Dio().get<List<dynamic>>(
+//         'http://my-json-server.typicode.com/adamsmaka/json-demo/users');
+//     final listDynamic = response.data;
+//     if (listDynamic == null) {
+//       return null;
+//     }
+//     // przechodzimy przez wszystkie dynamic i konwerttujemy go by
+//     // zwracało nam liste której potrzebujemy <List<Map<String, dynamic>>
+//     return listDynamic.map((e) => e as Map<String, dynamic>).toList();
+//   }
+// }
