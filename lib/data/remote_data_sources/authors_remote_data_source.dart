2167cb903ed462ef6887e4e7cf00d83b4f0bf2ec
Patrick <ledrap1992@gmail.com>
2023-01-18 13:13:34 +0100

    Add AuthorsRemoteDioDataSource

--- lib/data/remote_data_sources/authors_remote_data_source.dart
+++ lib/data/remote_data_sources/authors_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+
 class AuthorsMockedDataSource {
   Future<List<Map<String, dynamic>>?> getAuthors() async {
     return [
@@ -14,5 +16,20 @@ class AuthorsMockedDataSource {
         'last_name': 'Gdański',
       },
     ];
+  }
+}
+
+//przychodzi nam lista [] elementów
+class AuthorsRemoteDioDataSource {
+  Future<List<Map<String, dynamic>>?> getAuthors() async {
+    final response = await Dio().get<List<dynamic>>(
+        'http://my-json-server.typicode.com/adamsmaka/json-demo/users');
+    final listDynamic = response.data;
+    if (listDynamic == null) {
+      return null;
+    }
+    // przechodzimy przez wszystkie dynamic i konwerttujemy go by
+    // zwracało nam liste której potrzebujemy <List<Map<String, dynamic>>
+    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
   }
 }
